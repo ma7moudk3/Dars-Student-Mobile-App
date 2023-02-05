85a8ec9ec4807f7578dfbefd0340fd10e2e38ae1
ahmed_alashii <you@example.com>
2023-02-05 17:15:16 +0200

    Changes ..

--- lib/app/modules/edit_profile/views/edit_profile_view.dart
+++ lib/app/modules/edit_profile/views/edit_profile_view.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
+import '../../../../generated/locales.g.dart';
+import '../../../../global_presentation/global_widgets/custom_app_bar.dart';
+import '../../../constants/exports.dart';
 import '../controllers/edit_profile_controller.dart';
 
 class EditProfileView extends GetView<EditProfileController> {
@@ -10,9 +9,41 @@ class EditProfileView extends GetView<EditProfileController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('EditProfileView'),
-        centerTitle: true,
+      appBar: CustomAppBar(
+        title: LocaleKeys.personal_profile,
+        leading: GestureDetector(
+          onTap: () {
+            Get.back();
+          },
+          behavior: HitTestBehavior.opaque,
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: ColorManager.fontColor,
+            size: 20,
+          ),
+        ),
+        action: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () async {},
+          child: Container(
+            width: 20.w,
+            height: 20.h,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 2,
+                color: ColorManager.primary,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.check,
+                color: ColorManager.primary,
+                size: 16,
+              ),
+            ),
+          ),
+        ),
       ),
       body: const Center(
         child: Text(
